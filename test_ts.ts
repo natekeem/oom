e1f838d783f2e3e4fe430fc7c17ceab3861aa7b9
KIMSeongMin <2398973+KIMSeongMin@users.noreply.github.com>
2026-09-20 00:01:52 +0900

    chore: remove test file

--- test_ts.ts
+++
@@ -1,6 +0,0 @@
-import { SupabaseClient } from "@supabase/supabase-js";
-import { Database } from "./src/auth/authTypes";
-
-type T = Database['public']['Tables']['learning_sessions'];
-type InsertT = T['Insert'];
-type Check = InsertT extends Record<string, unknown> ? true : false;
